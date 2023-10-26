f89fb518edab1be3605d285bfbefdf6c79f492e3
Brad Johnson <brad@bradjohnson.io>
2023-10-26 09:29:34 -0400

    Added config

--- README.md
+++ README.md
@@ -10,7 +10,55 @@ TODO:
 
 - [ ] Mask Input
 - [ ] Enable Search
+- [x] Enable tags
+- [ ] Enable Metadata
 
 _This package is not affiliated with Cloudinary._
 
 **This package has been renamed from cld_get_assets to cld-get-asset**
+
+## Features
+
+- Fetch assets based on various parameters
+- Save assets information to a CSV file
+- Manage Cloudinary credentials
+
+## Installation
+
+To install the CLI tool globally, run:
+
+\`\`\`bash
+pnpm install -g cld-get-assets
+\`\`\`
+
+## Usage
+
+### Fetching Assets
+
+To fetch assets, simply run:
+
+\`\`\`bash
+cld-get-assets go
+\`\`\`
+
+Follow the prompts to enter your Cloudinary credentials and other settings.
+
+### Updating Credentials
+
+To update saved Cloudinary credentials, run:
+
+\`\`\`bash
+cld-get-assets config
+\`\`\`
+
+## Configuration
+
+The tool saves your Cloudinary credentials locally for future use. The config file is saved at `~/.cld_get_assets_config.json`.
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+
+This project is licensed under the ISC License.
--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,67 @@
 import { program } from 'commander';
 import * as fs from 'fs';
 import * as path from 'path';
+import * as os from 'os';
 import * as csv from 'fast-csv';
 import moment from 'moment';
 import prompt from 'prompt';
 import { v2 as cloudinary } from 'cloudinary';
+
+interface CloudinaryConfig {
+  cloud_name?: string;
+  api_key?: string;
+  api_secret?: string;
+}
+
+const config_path = path.join(os.homedir(), '.cld_get_assets_config.json');
+
+let saved_config: CloudinaryConfig;
+if (fs.existsSync(config_path)) {
+  saved_config = JSON.parse(fs.readFileSync(config_path, 'utf8'));
+}
+
+program
+  .command('config')
+  .description('Update Cloudinary credentials')
+  .action(() => {
+    prompt.start();
+    prompt.get(
+      ['cloud_name', 'api_key', 'api_secret'],
+      function (err: any, result: any) {
+        if (err) {
+          console.log(err);
+          process.exit(1);
+        }
+        const new_config = {
+          cloud_name: result.cloud_name,
+          api_key: result.api_key,
+          api_secret: result.api_secret,
+        };
+        fs.writeFileSync(config_path, JSON.stringify(new_config));
+        console.log('Credentials updated successfully.');
+      }
+    );
+  });
 
 program
   .command('go')
   .description('Fetch assets from Cloudinary')
   .action(() => {
     prompt.start();
-
     prompt.get(
-      [
-        'cloud_name',
-        'api_key',
-        'api_secret',
-        'output_directory',
-        'output_filename',
-      ],
+      {
+        properties: {
+          cloud_name: { default: saved_config?.cloud_name },
+          api_key: { default: saved_config?.api_key },
+          api_secret: { default: saved_config?.api_secret },
+          output_directory: { default: '.' },
+          output_filename: { default: 'assets.csv' },
+        },
+      },
       function (err: any, result: any) {
         if (err) {
-          if (err.message === 'canceled') {
-            console.log('Operation was canceled by the user. Exiting.');
-            process.exit(0);
-          } else {
-            console.log(err);
-            process.exit(1);
-          }
+          console.log(err);
+          process.exit(1);
         }
 
         cloudinary.config({
@@ -36,6 +69,13 @@ program
           api_key: result.api_key,
           api_secret: result.api_secret,
         });
+
+        const new_config = {
+          cloud_name: result.cloud_name,
+          api_key: result.api_key,
+          api_secret: result.api_secret,
+        };
+        fs.writeFileSync(config_path, JSON.stringify(new_config));
 
         const output_filename = result.output_file || 'assets.csv';
         const output_directory = result.output_directory || '.';
@@ -58,7 +98,8 @@ program
           try {
             const search_params = cloudinary.search
               .expression('')
-              .sort_by('public_id', 'desc')
+              .with_field('tags')
+              .sort_by('created_at', 'desc')
               .max_results(500);
 
             if (next_cursor) {
@@ -81,6 +122,7 @@ program
                 width: asset.width,
                 height: asset.height,
                 secure_url: asset.secure_url,
+                tags: asset.tags.join(','),
               });
             });
 
